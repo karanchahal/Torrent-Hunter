ea21f5ff6248738776d5064ee103e97cc8a0f805
Karanbir Chahal <karanbleep@gmail.com>
2016-10-22 01:36:54 +0530

    First commit on TorrentGrabber

---
+++ index.js
@@ -0,0 +1,2 @@
+magnetUri = "magnet:?xt=urn:btih:2e0c4217a839b0bd573cfd27be00baf94e92f56b&amp;dn=Star+Trek+Beyond+2016+1080p+Bluray+x264&amp;tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&amp;tr=udp%3A%2F%2Fzer0day.ch%3A1337&amp;tr=udp%3A%2F%2Fopen.demonii.com%3A1337&amp;tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&amp;tr=udp%3A%2F%2Fexodus.desync.com%3A6969"
+window.open(magnetUri);
